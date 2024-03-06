28ee4e9651075bed1aa263f18aaea8ddae767c51
talhajutt-01 <talhaj928@gmail.com>
2024-03-06 21:14:03 +0500

    check

--- src/Login.js
+++ src/Login.js
@@ -1,14 +1,24 @@
-// Login.js
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const Login = () => {
-  const { loginWithRedirect } = useAuth0();
+  const { loginWithRedirect, isAuthenticated } = useAuth0();
+
+  // Redirect to index.html if user is authenticated
+  React.useEffect(() => {
+    if (isAuthenticated) {
+      window.location.href = 'test/index.html';
+    }
+  }, [isAuthenticated]);
 
   return (
     <div>
       <h1>Login</h1>
-      <button onClick={() => loginWithRedirect()}>Login</button>
+      {isAuthenticated ? (
+        <p>Welcome! Redirecting to index.html...</p>
+      ) : (
+        <button onClick={() => loginWithRedirect()}>Login</button>
+      )}
     </div>
   );
 };
